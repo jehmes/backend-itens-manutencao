1d2cce637ec5aad292cd95858ae24050e3ee4426
Jehmes Thales <jeihmesthales@hotmail.com>
2022-09-28 18:57:24 -0300

    First commit

---
+++ src/main/java/com/br/api/manutencao/ApiItensManutencaoApplication.java
@@ -0,0 +1,13 @@
+package com.br.api.manutencao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiItensManutencaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiItensManutencaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/api/manutencao/ApiItensManutencaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.api.manutencao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiItensManutencaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
